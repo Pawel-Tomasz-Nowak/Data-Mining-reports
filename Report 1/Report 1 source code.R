fa107fd53175dd93b6d8be46215ba6dcd5127495
Pawel-Tomasz-Nowak <134939547+Pawel-Tomasz-Nowak@users.noreply.github.com>
2025-03-15 20:42:36 +0100

    I've loaded the dataset, recasted the variables to appropriate types and found variables with missing values.

---
+++ Report 1/Report 1 source code.R
@@ -0,0 +1,31 @@
+library(dplyr)
+library(tidyr)
+# Wczytywanie ramki danych.
+dane <- read.csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
+
+# Ile jest wierszy, a ile - zmiennych?
+n.row <- nrow(dane) # 7043 wierszy.
+n.col <- ncol(dane) # 21 kolumn.
+
+
+
+# Dane zawierają sporo zmiennych jakościowych nieporzadkowych, które zostały błędnie wczytane jako zmienne typu napis. 
+vars.to.cast <- c("gender", "SeniorCitizen", "Partner", "Dependents", "PhoneService", "MultipleLines",
+                  "InternetService", "OnlineSecurity", "OnlineBackup", "DeviceProtection",
+                  "TechSupport", "StreamingTV", "StreamingMovies", "Contract", "PaperlessBilling",
+                  "PaymentMethod", "Churn")
+
+# Dokonaj konwersji na typ `factor`.
+dane[vars.to.cast] <- lapply(dane[vars.to.cast], as.factor)
+
+
+
+# Znajdź cechy, które zawierają (standardowo kodowane) wartości brakujące
+n.missing <- dane %>% 
+  summarize(across(everything(), function(x) { sum(is.na(x)) } ) ) %>% 
+  pivot_longer( everything(), names_to = "variable", values_to = "na.count" ) %>%
+  filter( na.count > 0)
+
+             
+# Jest tylko jedna taka zmienna - "TotalCharges".
+print(n.missing)
