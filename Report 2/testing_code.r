08eb398a92c578f6e8a9fabd6878dc7168e0087c
Pawel-Tomasz-Nowak <134939547+Pawel-Tomasz-Nowak@users.noreply.github.com>
2025-04-04 20:21:18 +0200

    Implemented a few discretization methods (equal-width, equal-frequency, clustering, fixed)

--- Report 2/testing_code.r
+++ Report 2/testing_code.r
@@ -3,23 +3,69 @@ library(dplyr)
 library(ggplot2)
 library(GGally)
 library(gridExtra)
+library(arules)
 data("iris") # Wczytywanie danych
 df <- iris
 
 
 num.cols <- colnames(df %>% select( where(is.numeric)))
+# 
+# violin.plot.box_maker <- function(var){
+# ggplot(data = df, 
+#        aes(x = Species, y = .data[[var]],fill = Species)) +
+#   geom_violin(alpha = 0.2) +
+#   geom_boxplot(width = 0.15)
+# }
+# 
+# grid.arrange(violin.plot.box_maker("Sepal.Length"),
+#              violin.plot.box_maker("Sepal.Width"),
+#              violin.plot.box_maker("Petal.Length"),
+#              violin.plot.box_maker("Petal.Width"))
+# 
+#              
 
-violin.plot.box_maker <- function(var){
-ggplot(data = df, 
-       aes(x = Species, y = .data[[var]],fill = Species)) +
-  geom_violin(alpha = 0.2) +
-  geom_boxplot(width = 0.15)
+var.discretization <- function(df, var){
+  # Estimate the mean and sd of the variable (we'll use these values for fixed-type discretization)
+  var.vec <- df[[var]]
+  
+  mean.est <- mean(var.vec)
+  sd.est <- sd(var.vec)
+  
+  
+  # Oparta na równej częstotliwości.
+  var.discr.freq <- discretize(var.vec, 
+                               method = "frequency", 
+                               breaks = 3,
+                               label = c("Krótka", "Średnia", "Długa"))
+  
+  # equal-width discretization
+  var.discr.width <- discretize(var.vec, 
+                                method = "interval", 
+                                breaks = 3,
+                                label = c("Krótka", "Średnia", "Długa"))
+  
+  # cluster discretization
+  var.discr.cluster <- discretize(var.vec, 
+                                  method = "cluster", 
+                                  breaks = 3,
+                                  label = c("Krótka", "Średnia", "Długa"))
+  
+  # user-specified breaks bounds.
+  var.discr.user <- discretize(var.vec,
+                               method = "fixed",
+                               breaks = c(-Inf, mean.est-sd.est, mean.est+sd.est, Inf),
+                               label = c("Krótka", "Średnia", "Długa"))
+
+  
+  
+  # Store discretization result in a dataframe.
+  df.discr_vals <- data.frame("equal-frequency" = var.discr.freq,
+                              "equal-width" = var.discr.width,
+                              "cluster-based" = var.discr.cluster,
+                              "fixed-bounds"= var.discr.user
+                              )
+  
 }
 
-grid.arrange(violin.plot.box_maker("Sepal.Length"),
-             violin.plot.box_maker("Sepal.Width"),
-             violin.plot.box_maker("Petal.Length"),
-             violin.plot.box_maker("Petal.Width"))
 
-             
 
