8c3fafedf7de089b365979c4959adf6f476d4cf9
Pawel-Tomasz-Nowak <134939547+Pawel-Tomasz-Nowak@users.noreply.github.com>
2025-04-05 20:45:19 +0200

    Loaded the City Quality of Life Dataset; plotted boxplots for all quantitative variables; created the section for PCA.

--- Report 2/testing_code.r
+++ Report 2/testing_code.r
@@ -4,14 +4,18 @@ library(ggplot2)
 library(GGally)
 library(gridExtra)
 library(arules)
+library(knitr)
+library(tidyr)
 data("iris") # Wczytywanie danych
 df <- iris
 
 
-num.cols <- colnames(df %>% select( where(is.numeric)))
+# ZADANIE 1
+
+# num.cols <- colnames(df %>% select( where(is.numeric)))
 # 
 # violin.plot.box_maker <- function(var){
-# ggplot(data = df, 
+# ggplot(data = df,
 #        aes(x = Species, y = .data[[var]],fill = Species)) +
 #   geom_violin(alpha = 0.2) +
 #   geom_boxplot(width = 0.15)
@@ -22,61 +26,79 @@ num.cols <- colnames(df %>% select( where(is.numeric)))
 #              violin.plot.box_maker("Petal.Length"),
 #              violin.plot.box_maker("Petal.Width"))
 # 
-#              
+# 
+# 
+# var.discretization <- function(df, var){
+#   # Estimate the mean and sd of the variable (we'll use these values for fixed-type discretization)
+#   var.vec <- df[[var]]
+#   
+#   mean.est <- mean(var.vec)
+#   sd.est <- sd(var.vec)
+#   
+#   
+#   # Oparta na równej częstotliwości.
+#   var.discr.freq <- discretize(var.vec, 
+#                                method = "frequency", 
+#                                breaks = 3,
+#                                label = c("Krótka", "Średnia", "Długa"))
+#   
+#   # equal-width discretization
+#   var.discr.width <- discretize(var.vec, 
+#                                 method = "interval", 
+#                                 breaks = 3,
+#                                 label = c("Krótka", "Średnia", "Długa"))
+#   
+#   # cluster discretization
+#   var.discr.cluster <- discretize(var.vec, 
+#                                   method = "cluster", 
+#                                   breaks = 3,
+#                                   label = c("Krótka", "Średnia", "Długa"))
+#   
+#   # user-specified breaks bounds.
+#   var.discr.user <- discretize(var.vec,
+#                                method = "fixed",
+#                                breaks = c(-Inf, mean.est-sd.est, mean.est+sd.est, Inf),
+#                                label = c("Krótka", "Średnia", "Długa"))
+# 
+#   
+#   
+#   # Store discretization result in a dataframe.
+#   df.discr_vals <- data.frame("equal-frequency" = var.discr.freq,
+#                               "equal-width" = var.discr.width,
+#                               "cluster-based" = var.discr.cluster,
+#                               "fixed-bounds"= var.discr.user
+#                               )
+#   
+# }
+# 
+# 
+# # Przykładowy wektor liczb
+# vec <- c(1, 2, 3, 4, 5)
+# 
+# # Zamiana wektora na ramkę danych
+# df <- data.frame(Wartości = vec)
+# 
+# # Tworzenie tabeli za pomocą knitr::kable
+# kable(df, caption = "Tabela z wektora liczb")
 
-var.discretization <- function(df, var){
-  # Estimate the mean and sd of the variable (we'll use these values for fixed-type discretization)
-  var.vec <- df[[var]]
-  
-  mean.est <- mean(var.vec)
-  sd.est <- sd(var.vec)
-  
-  
-  # Oparta na równej częstotliwości.
-  var.discr.freq <- discretize(var.vec, 
-                               method = "frequency", 
-                               breaks = 3,
-                               label = c("Krótka", "Średnia", "Długa"))
-  
-  # equal-width discretization
-  var.discr.width <- discretize(var.vec, 
-                                method = "interval", 
-                                breaks = 3,
-                                label = c("Krótka", "Średnia", "Długa"))
-  
-  # cluster discretization
-  var.discr.cluster <- discretize(var.vec, 
-                                  method = "cluster", 
-                                  breaks = 3,
-                                  label = c("Krótka", "Średnia", "Długa"))
-  
-  # user-specified breaks bounds.
-  var.discr.user <- discretize(var.vec,
-                               method = "fixed",
-                               breaks = c(-Inf, mean.est-sd.est, mean.est+sd.est, Inf),
-                               label = c("Krótka", "Średnia", "Długa"))
+# Zadanie 2.
 
-  
-  
-  # Store discretization result in a dataframe.
-  df.discr_vals <- data.frame("equal-frequency" = var.discr.freq,
-                              "equal-width" = var.discr.width,
-                              "cluster-based" = var.discr.cluster,
-                              "fixed-bounds"= var.discr.user
-                              )
-  
-}
-
-library(knitr)
-
-# Przykładowy wektor liczb
-vec <- c(1, 2, 3, 4, 5)
-
-# Zamiana wektora na ramkę danych
-df <- data.frame(Wartości = vec)
-
-# Tworzenie tabeli za pomocą knitr::kable
-kable(df, caption = "Tabela z wektora liczb")
+# Wczytywanie danych.
+CQL_df <- read.csv(file = "uaScoresDataFrame.csv") %>%
+            mutate(X = NULL)
 
 
+# Wybierz same cechy ilościowe.
+num_cols <- colnames(select(CQL_df, where(is.numeric)))
+
+CQL_df_longer <- CQL_df %>% pivot_longer( num_cols, values_to = "Realizacja", names_to = "Zmienna")
+
+
+ggplot(CQL_df_longer, aes(y = Realizacja, fill = Zmienna)) +
+  geom_boxplot() +
+  theme(axis.text.y = element_text(angle = 45, hjust = 1),
+        axis.text.x = element_blank()) +
+  labs(title = "Wykres pudełkowy zmiennych ilościowych",
+       y = "Wartości")+
+  guides(fill = guide_legend(title = NULL)) 
 
